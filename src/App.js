fb2c257c77c4151ebefc65257398bdfc48eef10e
sbcreations14378 <dsak@gmail.com>
2023-09-27 00:27:19 +0530

    Huff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import News from './components/News'
-import { Route,Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<News/>}/>
-      </Routes>
       
+      <News/>
+
     </div>
-  )
+  );
 }
+
 
 export default App
