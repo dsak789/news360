37e41a955be4d6e31cca48672c325387cd36441c
sbcreations14378 <dsak@gmail.com>
2023-09-28 16:29:56 +0530

    Required changes

--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,8 @@ const News = () => {
   const [error, setError] = useState(null);
 
   const news_data = () => {
-    fetch('http://newsapi.org/v2/top-headlines?country=in&apiKey=194bc3ea605b48bf96cc64cab7eec7cb')
+    const api ='https://inshortsapi.vercel.app/news?category=all';
+    fetch(api)
       .then((res) => {
         if (!res.ok) {
           throw new Error('Network response was not ok');
@@ -16,8 +17,8 @@ const News = () => {
         return res.json();
       })
       .then((data) => {
-        console.log(data.articles);
-        setNdata(data.articles);
+        console.log(data.data);
+        setNdata(data.data);
       })
       .catch((error) => {
         console.error('Error fetching news data:', error);
@@ -40,21 +41,21 @@ const News = () => {
             ndata.map((info) => (
               <div key={info.url} className='news'>
                 <div className='news-image'>
-                  <img className='news-img' height={250} width={250} src={info.urlToImage} alt='' />
+                  <img className='news-img' height={250} width={250} src={info.imageUrl} alt='' />
                 </div>
                 <div className='news-content'>
                   <div className='headline'>{info.title}</div>
                   <div className='news-story'>
-                    {info.description}
-                    <br />
                     {info.content}
-                    <a href={info.url} target='_blank' rel='noopener noreferrer'>
+                    {/* <br /> */}
+                    {/* {info.content} */}
+                    <a href={info.readMoreUrl} target='_blank' rel='noopener noreferrer'>
                       Readmore<FaArrowRight />
                     </a>
                   </div>
                   <div className='post-details'>
                     <div className='posted-by'>Post By: {info.author}</div>
-                    <div className='date-time'>Posted on: {info.publishedAt}</div>
+                    <div className='date-time'>Posted on: {info.date} {info.time}</div>
                   </div>
                 </div>
               </div>
