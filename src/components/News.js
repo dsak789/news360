58cba684cc891a5bcb66613db9a2a386711fee3d
dsak789 <dsak.official@gmail.com>
2024-08-17 02:35:19 +0530

    Added New News API and Made Responsive

--- src/components/News.js
+++ src/components/News.js
@@ -3,37 +3,23 @@ import './css/News.css';
 import Nav from './Nav';
 import { FaArrowRight, FaGithub, FaLinkedin } from 'react-icons/fa';
 import axios from 'axios';
+import logo from './Images/My Logo Balck.png'
 
 const News = () => {
   const [ndata, setNdata] = useState([]);
   const [error, setError] = useState(null);
 
   const news_data = async() => {
-    // const api ='https://inshortsapi.vercel.app/news?category=all';
-    // const api ='https://newsapi.org/v2/everything?q=tesla&apiKey=194bc3ea605b48bf96cc64cab7eec7cb'
-    // fetch(api)
-    //   .then((res) => {
-    //     if (!res.ok) {
-    //       throw new Error('Network response was not ok');
-    //     }
-    //     return res.json();
-    //   })
-    //   .then((data) => {
-    //     console.log(data.data);
-    //     setNdata(data.data);
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error fetching news data:', error);
-    //     setError('Failed to fetch news data. Please try again later.');
-    //   });
+    const api ='https://newsapi.org/v2/everything?q=tesla&apiKey=194bc3ea605b48bf96cc64cab7eec7cb'
+    
    try{
-        const response = await axios.get('https://360newstest.vercel.app/all')
+        const response = await axios.get(api)
         
         if(response){
             // console.log("Local Api Feteched"+response.data)
             const news=(response)    
-            console.log(news.data)
-            setNdata(news.data)
+            // console.log(news.data.articles)
+            setNdata(news.data.articles)
         }
    }
    catch(error){
@@ -47,51 +33,54 @@ const News = () => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <div className='all-news'>
-          <Nav />
-          {error ? (
-            <div className='error-message'>{error}</div>
-          ) : (
-            ndata.map((info) => (
-              <div key={info.title} className='news'>
-                <div className='news-image'>
-                  <img className='news-img' height={250} width={250} src={info.imageUrl} alt='' />
-                </div>
-                <div className='news-content'>
-                  <div className='headline'>{info.title}</div>
-                  <div className='news-story'>
-                    {info.content}
-                    <br />
-                    {/* {info.description} */}
-                    <a href={info.readMoreUrl} target='_blank' rel='noopener noreferrer'>
-                      Readmore<FaArrowRight />
-                    </a>
-                  </div>
-                  <div className='post-details'>
-                    <div className='posted-by'>Post By: {info.author}</div>
-                    <div className='date-time'>Posted on: {info.date} {info.time}</div>
-                  </div>
-                </div>
-              </div>
-            ))
-          )}
-          <div className='footer' id='footer'>
-            <div className='icons'>
-              <a href='https://github.com/sbcreations14378'>
-                <FaGithub />
-              </a>
-              &nbsp;&nbsp;&nbsp;
-              <a href='https://www.linkedin.com/in/'>
-                <FaLinkedin />{' '}
-              </a>
+    <div className='all-news'>
+      <Nav />
+      {error ? (
+        <div className='error-message'>{error}</div>
+      ) : (
+        ndata.map((info,index) => (
+          
+            info.author != null ?  
+          (
+            
+          <div key={index} className='news'>
+            <div className='news-image'>
+              
+              <img className='news-img' height={250} width={250} src={(info.urlToImage)!=null? info.urlToImage : logo} alt='' />
             </div>
-            <p>
-              Made by Dannana Sai Ajith Kumar | Copyright &copy; by SAK789 | All rights reserved
-            </p>
-          </div>
+            <div className='news-content'>
+              <div className='headline'>{info.title}</div>
+              <div className='news-story'>
+                {info.content}
+                <br />
+                {/* {info.description} */}
+                <a href={info.url} target='_blank' rel='noopener noreferrer'>
+                  Readmore<FaArrowRight />
+                </a>
+              </div>
+              <div className='post-details'>
+                <div className='posted-by'>Post By: {info.author}</div>
+                <div className='date-time'>Posted on: {info.publishedAt} {info.time}</div>
+              </div>
+            </div>
+          </div>)
+          :
+          <></>
+        ))
+      )}
+      <div className='footer' id='footer'>
+        <div className='icons'>
+          <a href='https://github.com/dsak789'>
+            <FaGithub />
+          </a>
+          &nbsp;&nbsp;&nbsp;
+          <a href='https://www.linkedin.com/in/saiajithkumardannana'>
+            <FaLinkedin />{' '}
+          </a>
         </div>
+        <p>
+          Made by Dannana Sai Ajith Kumar | Copyright &copy; by DSAK789 | All rights reserved
+        </p>
       </div>
     </div>
   );
--- src/components/NewsApi.js
+++ src/components/NewsApi.js
@@ -1,27 +1,85 @@
-const axios = require('axios')
-const express = require('express')
-const cors = require('cors');
-const app =express()
-app.use(cors());
- app.use(express.json())
+import React, { useEffect, useState } from 'react';
+import './css/News.css';
+import Nav from './Nav';
+import { FaArrowRight, FaGithub, FaLinkedin } from 'react-icons/fa';
+import axios from 'axios';
 
-// const [ndata,setNdata] = useState([])
+const News = () => {
+  const [ndata, setNdata] = useState([]);
+  const [error, setError] = useState(null);
 
+  const news_data = async() => {
+    const api ='https://inshortsapi.vercel.app/news?category=all';
+    fetch(api)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data.data);
+        setNdata(data.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching news data:', error);
+        setError('Failed to fetch news data. Please try again later.');
+      });
+  };
 
-app.get('/all',async (req,res)=>{
-    try{
-        const response= await axios.get('https://inshortsapi.vercel.app/news?category=all');
-        const news=response.data;
-        // console.log(news)
-        res.status(200).json(news.data) 
-    
-    }
-    catch(error){
-        console.log("Error in Fteching Neews API", error)
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-})
+  useEffect(() => {
+    news_data();
+  }, []);
 
-app.listen(1437,(req,res)=>{
-    console.log("Port running at no:1437")
-})
+  return (
+    <div>
+      <div>
+        <div className='all-news'>
+          <Nav />
+          {error ? (
+            <div className='error-message'>{error}</div>
+          ) : (
+            ndata.map((info) => (
+              <div key={info.title} className='news'>
+                <div className='news-image'>
+                  <img className='news-img' height={250} width={250} src={info.imageUrl} alt='' />
+                </div>
+                <div className='news-content'>
+                  <div className='headline'>{info.title}</div>
+                  <div className='news-story'>
+                    {info.content}
+                    <br />
+                    {/* {info.description} */}
+                    <a href={info.readMoreUrl} target='_blank' rel='noopener noreferrer'>
+                      Readmore<FaArrowRight />
+                    </a>
+                  </div>
+                  <div className='post-details'>
+                    <div className='posted-by'>Post By: {info.author}</div>
+                    <div className='date-time'>Posted on: {info.date} {info.time}</div>
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
+          <div className='footer' id='footer'>
+            <div className='icons'>
+              <a href='https://github.com/dsak789'>
+                <FaGithub />
+              </a>
+              &nbsp;&nbsp;&nbsp;
+              <a href='https://www.linkedin.com/in/saiajithkumardannana'>
+                <FaLinkedin />{' '}
+              </a>
+            </div>
+            <p>
+              Made by Dannana Sai Ajith Kumar | Copyright &copy; by SAK789 | All rights reserved
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default News;
